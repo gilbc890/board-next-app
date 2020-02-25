196f68b58bbb8b3d76cb5c6676e9bd69a4a39871
Byungchan Gil <gilbc890@gmail.com>
2020-02-25 12:15:42 -0600

    circleCi test

--- components/Main/index.js
+++ components/Main/index.js
@@ -10,7 +10,6 @@ const Main = () => (
         <main className="main-container">
             <WeeklyAside/>
             <HumorBoard/>
-            <Upload/>
         </main>
         <style jsx>{`
             .main-container{
