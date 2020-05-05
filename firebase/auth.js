62ab5ae249d7ce91fba30104c0184fa0f760572b
Byungchan Gil <gilbc890@gmail.com>
2020-05-04 20:38:18 -0500

    save user data

--- firebase/auth.js
+++ firebase/auth.js
@@ -1,11 +1,34 @@
 import firebase from 'firebase/app';
-import { auth } from './';
 
 export const authWithFacebook = async () => {
   const provider = new firebase.auth.FacebookAuthProvider();
-  auth.signInWithPopup(provider)
+  const result = await firebase.auth().signInWithPopup(provider);
+  await signInWithProvider(result);
 };
 
+const signInWithProvider = async (result) => {
+  const { user } = result;
+  const userExists = await getUserById(user.uid);
+  
+  if(!userExists) {
+    const ref = await firebase.database().ref('users/'+user.uid);
+
+    ref.update({
+        "timestamp": new Date().getTime(),
+        "id": user.uid,
+        "providerData":user.providerData,
+    })  
+  } else{
+    return;
+  }
+};
+
+export const getUserById = async (userId) => {
+  const user = await firebase.database().ref(`users/${userId}`).once('value').then((snapshot) => snapshot.val());
+  return user;
+};
+
+
 export const signOut = async () => {
-  auth.signOut().then(window.location.reload());
+  firebase.auth().signOut().then(window.location.reload());
 };
