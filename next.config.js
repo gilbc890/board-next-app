55ee56c141cc7e233c1b4f447ad439cfb9a0db45
Byungchan Gil <gilbc890@gmail.com>
2020-05-05 07:54:42 -0500

    adjusting dotenv

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-const withCSS = require('@zeit/next-css')
-module.exports = withCSS({
+const webpack = require('webpack')
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { parsed: localEnv } = require('dotenv').config()
+
+module.exports = {
   /* config options here */
-})
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
+
+    return config
+  }
+}
